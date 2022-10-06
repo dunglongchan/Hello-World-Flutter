6e0229dcd97240d2010cc5ae0ea71a341e349f65
buidangnambinh <bdn.binh251001@gmail.com>
2022-10-06 23:02:04 +0700

    add layout

--- momo/app/src/main/java/com/example/momo/ViewpagerAdapter.kt
+++ momo/app/src/main/java/com/example/momo/ViewpagerAdapter.kt
@@ -4,13 +4,14 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
     override fun getItemCount(): Int {
         return 3
     }
 
     override fun createFragment(position: Int): Fragment {
-        when(position){
+        when (position) {
             1 -> return VoucherFragment()
             2 -> return QrCodeFragment()
             3 -> return ChatBoxFragment()
